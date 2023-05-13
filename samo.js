188f22e58c1e8174b94dec6c154818cd79ced765
aladeen <divsri9211@gmail.com>
2023-05-13 15:45:19 +0530

    learned what variable is

--- samo.js
+++ samo.js
@@ -13,3 +13,4 @@ const path = require('path')
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
---
+++ variable.js
@@ -0,0 +1,8 @@
+var a = 45 // contains 45
+console.log(a)
+
+// no. are not alowed while taking variable
+
+//var 9hey = 67
+//console.log(hey6)
+
