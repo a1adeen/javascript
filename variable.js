0ead6e495777c0cfc9b02e71b25b0a536898c473
aladeen <divsri9211@gmail.com>
2023-05-14 13:59:47 +0530

    made alert button using js

--- variable.js
+++ variable.js
@@ -6,3 +6,10 @@ console.log(a)
 //var 9hey = 67
 //console.log(hey6)
 
+let clickmebtn = document.querySelector('button');
+clickmebtn.addEventListener('click', inputmsg);
+
+function inputmsg(){
+    let name = prompt('enter the Name of student');
+    clickmebtn.textContent = 'roll no.1' + name;
+}
